d288740f5b492f68b5762c31138be84ea6dc47f0
Pratik Das <67233000+pratik2050@users.noreply.github.com>
2022-01-31 19:30:26 +0530

    added popup js

--- js/onclick.js
+++ js/onclick.js
@@ -7,3 +7,29 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+
+
+// Popup JS
+const popupScreen = document.querySelector(".popup-screen");
+const popuoBox = document.querySelector(".popup-box");
+const closeBtn = document.querySelector(".close-btn");
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        popupScreen.classList.add("active");
+    }, 2000);
+});
+
+closeBtn.addEventListener("click", () => {
+    popupScreen.classList.remove("active");
+
+    document.cookie = "WebsiteName = ANARC; max-age=" + 24*60*60;
+});
+
+const WebsiteCookie = document.cookie.indexOf("WebsiteName=");
+
+if (WebsiteCookie != -1) {
+    popupScreen.style.display = "none";
+} else {
+    popupScreen.style.display = "flex";
+}
